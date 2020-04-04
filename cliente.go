4aa779241172065c4492dd5a9f13619ca4673fd5
gustavo <gustavodevaler@gmail.com>
2020-04-04 14:57:36 -0300

    Cliente en golan

---
+++ cliente.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Client struct {
+}
+type Connexion struct {
+}
+type Message struct {
+}
+
+func main() {
+	protocol := "TCP"
+	server := "irc.freenode.net"
+	port := 6667
+	channel := "#bitcoin"
+	username := "Gambito123"
+	msg := " :Hola !"
+
+}

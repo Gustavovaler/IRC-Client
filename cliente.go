84cdc26ea2d14317f75868291000f13eaa4073e5
gustavo <gustavodevaler@gmail.com>
2020-04-04 15:52:03 -0300

    Bot Golang

--- cliente.go
+++ cliente.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"net/textproto"
+	"strings"
 )
 
-type Client struct {
-}
-type Connexion struct {
-}
-type Message struct {
-}
-
 func main() {
-	protocol := "TCP"
-	server := "irc.freenode.net"
-	port := 6667
-	channel := "#bitcoin"
-	username := "Gambito123"
-	msg := " :Hola !"
+	protocol := "tcp"
+	server := "irc.freenode.net:6667"
+
+	//Inicio la conexion
+	conn, err := net.Dial(protocol, server)
+	if err != nil {
+		panic(err)
+	}
+	// envio las credenciales
+
+	fmt.Fprintf(conn, "USER %s 8 * :%s\r\n", "GustavoKirchbnet", "GustavoKirchbnet")
+	fmt.Fprintf(conn, "NICK %s\r\n", "NestorKirchner")
+	fmt.Fprintf(conn, "JOIN %s\r\n", "#bitcoin")
+
+	// Cpturamos la respuesta
+	reader := bufio.NewReader(conn)
+	// Formateamos a string
+	tp := textproto.NewReader(reader)
+
+	loading := false
+
+	for loading == false {
+		// Mostramos por pantalla
+		line, err := tp.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("%s\n", line)
+		// Evaluamos el estado de la conexion
+		if strings.Contains(line, "/NAMES list") {
+			loading = true
+		}
+
+	}
+	//enviamos mensaje
+
+	fmt.Fprintf(conn, "PRIVMSG #bitcoin  :Hola !\r\n")
 
 }
